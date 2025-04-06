86414188eabd67bcedce966f492efbdcc4908aba
Muhammad <bryanedwarding@gmail.com>
2025-04-06 22:29:07 +0100

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
-import { navzLinks } from "../constants";
+import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
